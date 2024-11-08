c3decb743292c6db4d22d9c3b45f40eaa0f703b8
alex_ <igrigoriev60@gmail.com>
2024-11-08 11:26:56 +0500

    вместо кнопки добавлено меню

--- cataas.py
+++ cataas.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+from pandas.core.window import Window
 from pygame.display import update
 
 
@@ -22,8 +23,11 @@ def load_image(url):
 def set_image():
     img = load_image(url)
     if img:
-        label.config(image=img)
+        label.config(image = img)
         label.image = img
+
+def exit():
+    window.destroy()
 
 window = Tk()
 window.title('Cats!')
@@ -32,8 +36,18 @@ window.geometry('600x520')
 
 label = Label()
 label.pack()
-update_button = Button(text = 'Обновить картинку', command=set_image)
-update_button.pack()
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar,tearoff=0)
+menu_bar.add_cascade(label='Файл', menu = file_menu)
+file_menu.add_command(label='Загрузить фото', command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label='Выход',command=exit)
+
+# update_button = Button(text = 'Обновить картинку', command=set_image)
+# update_button.pack()
 
 url = 'https://cataas.com/cat'
 
