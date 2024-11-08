43c31e2892ed6e545ec9d98a3e24afcaaffe94ea
alex_ <igrigoriev60@gmail.com>
2024-11-08 11:21:47 +0500

    Создана кнопка для обновления картинки и установили предел размера картинки

--- cataas.py
+++ cataas.py
@@ -3,36 +3,40 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from bottle import response
-from pygame.examples.aliens import load_image
-from pygame.examples.sprite_texture import load_img
+from pygame.display import update
 
 
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600,480),Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f'Произошла ошибка: {e}')
         return None
 
 
+def set_image():
+    img = load_image(url)
+    if img:
+        label.config(image=img)
+        label.image = img
+
 window = Tk()
 window.title('Cats!')
-window.geometry('600x480')
+window.geometry('600x520')
 
 
 label = Label()
 label.pack()
+update_button = Button(text = 'Обновить картинку', command=set_image)
+update_button.pack()
 
-url = 'https//cataas.com/cat'
-img = load_image(url)
+url = 'https://cataas.com/cat'
 
-if img:
-    label.config(image=img)
-    label.image = img
+set_image()
 
 window.mainloop()
