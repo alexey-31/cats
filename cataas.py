287deb004c112cf25be02ed825668ab9f042ef72
alex_ <igrigoriev60@gmail.com>
2024-11-08 11:01:27 +0500

    Создано окно и виджеты

--- cataas.py
+++ cataas.py
@@ -1,1 +1,24 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+from pygame.examples.aliens import load_image
+from pygame.examples.sprite_texture import load_img
+
+window = Tk()
+window.title('Cats!')
+window.geometry('600x480')
+
+
+label = Label()
+label.pack()
+
+url = 'https//cataas.com/cat'
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
